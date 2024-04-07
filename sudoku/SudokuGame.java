823d8758a4e067aac5a1580a304cd042f04b4c90
tngkrwlsmd <wjsdudals7590@naver.com>
2024-04-07 23:08:18 +0900

    패키지 수정

--- sudoku/SudokuGame.java
+++ sudoku/SudokuGame.java
@@ -1,4 +1,4 @@
-package project.sudoku;
+package sudoku;
 import javax.swing.*;
 import javax.swing.Timer;
 import javax.swing.event.*;
--- sudoku/SudokuSolver.java
+++ sudoku/SudokuSolver.java
@@ -1,4 +1,4 @@
-package project.sudoku;
+package sudoku;
 import java.io.*;
 import java.util.Arrays;
 
